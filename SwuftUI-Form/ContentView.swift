3ecc04fd8512ec69415d94bba70f1fc02597fa8e
EdneyMesquita <edneyphp@outlook.com>
2021-07-29 15:33:58 -0300

    Initial Commit

---
+++ SwuftUI-Form/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwuftUI-Form
+//
+//  Created by Edney Mesquita on 29/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwuftUI-Form/SwuftUI_FormApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwuftUI_FormApp.swift
+//  SwuftUI-Form
+//
+//  Created by Edney Mesquita on 29/07/21.
+//
+
+import SwiftUI
+
+@main
+struct SwuftUI_FormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
